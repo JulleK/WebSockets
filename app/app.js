9ad15f85c33a02090f87134a033051c84a5bcba5
Julek <julek.krzeszycha@gmail.com>
2024-04-19 19:03:10 +0200

    create basic frontend js ws connection

---
+++ app/app.js
@@ -0,0 +1,23 @@
+const ws = new WebSocket("ws://localhost:8080");
+
+const messageForm = document.querySelector(".message-form");
+const messageDisplay = document.querySelector("#messages");
+
+messageForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let userMessage = event.target[0].value;
+  ws.send(userMessage);
+});
+
+ws.addEventListener("open", () => {
+  console.log("connection to wss open");
+});
+
+ws.addEventListener("message", (event) => {
+  let message = event.data;
+  messageDisplay.innerHTML += `<li class="message">${message}</li>`;
+});
+
+ws.addEventListener("close", () => {
+  console.log("Connection closed");
+});
