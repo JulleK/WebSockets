bd5427017915e310c2d5ad2f3f31a8bca96c62aa
Julek <julek.krzeszycha@gmail.com>
2024-04-22 18:41:05 +0200

    write basic socket server

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 import express from "express";
+import { createServer } from "http";
 import path from "path";
 import { fileURLToPath } from "url";
+import { Server } from "socket.io";
 
-const app = express();
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
+const app = express();
 app.use(express.static(path.join(__dirname, "public")));
+const server = createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views/index.html"));
